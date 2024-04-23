65ba35775edf9d5d775c535a20dc0219513e58dc
Kyuubi <khoi.nv202647@sis.hust.edu.vn>
2024-04-23 20:04:59 +0700

    Makefile_general

---
+++ general_knowledge/Makefile_Genreral/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <hello.h>
+void hello()
+{
+	printf("it's hello from header file\n");
+}
---
+++ general_knowledge/Makefile_Genreral/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include "hello.h"
+void main(){
+	printf("Hello_World\n");
+	hello();
+}
